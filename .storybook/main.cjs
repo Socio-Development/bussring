c46b34822a9ceaa17221c63c0dbf6414680c2e59
CasperSocio <casper.tollefsen@mobit.no>
2021-10-20 15:30:36 +0200

    initial commit

---
+++ .storybook/main.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  core: {
+    builder: "storybook-builder-vite"
+  },
+  stories: [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-svelte-csf"
+  ],
+  svelteOptions: {
+    preprocess: require("../svelte.config.cjs").preprocess
+  }
+}
