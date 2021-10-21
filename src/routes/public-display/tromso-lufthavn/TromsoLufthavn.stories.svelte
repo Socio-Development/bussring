49977d973321a2a102b869825f88fe1fa96c7a60
Casper Tollefsen <casper.tollefsen@mobit.no>
2021-10-21 13:29:08 +0200

    add 'Tromsø Lufthavn' route

---
+++ src/routes/public-display/tromso-lufthavn/TromsoLufthavn.stories.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
+  import Page from './index.svelte'
+</script>
+
+<Meta
+  title="Routes/PublicDisplay"
+  component={Page}
+/>
+
+<Story name="TromsoLufthavn">
+  <Page />
+</Story>
---
+++ src/routes/public-display/tromso-lufthavn/index.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+  <title>Trosmo Lufthavn</title>
+</svelte:head>
+
+<h1>Tromsø Lufthavn</h1>
