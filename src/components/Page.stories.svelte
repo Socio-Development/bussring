4985117c9b1928cc6214a5779de24bb393867940
Casper Tollefsen <casper.tollefsen@mobit.no>
2021-10-21 13:29:32 +0200

    create a page component

---
+++ src/components/Page.stories.svelte
@@ -0,0 +1,33 @@
+<script>
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
+  import Page from './Page.svelte'
+</script>
+
+<Meta
+  title="Components/Page"
+  component={Page}
+  argTypes={{
+    location: {
+      control: 'text',
+      name: 'Location'
+    },
+    title: {
+      control: 'text',
+      name: 'Title'
+    }
+  }}
+/>
+
+<Template let:args>
+  <Page
+    {...args}
+  />
+</Template>
+
+<Story
+  name="TromsoLufthavn"
+  args={{
+    location: 'Tromsø Lufthavn',
+    title: 'Tromsø Lufthavn'
+  }}
+/>
---
+++ src/components/Page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let location: string
+  export let title: string
+</script>
+
+<svelte:head>
+  <title>{ title }</title>
+</svelte:head>
+
+<h1 class="border">{ location }</h1>
