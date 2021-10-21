8587ae4a29a66c7b203e2e42b38671e85c43b6cb
Casper Tollefsen <casper.tollefsen@mobit.no>
2021-10-21 20:14:44 +0200

    add Table component

---
+++ src/components/Table.stories.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import {
+    Meta,
+    Story,
+    Template
+  } from '@storybook/addon-svelte-csf'
+  import Table from './Table.svelte'
+</script>
+
+<Meta
+  title="Components/Table"
+  component={Table}
+/>
+
+<Template let:args>
+  <Table
+    {...args}
+  />
+</Template>
+
+<Story
+  name="Default"
+/>
---
+++ src/components/Table.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+  interface TableBody {
+    time: string,
+    dest: string
+  }
+  interface TableData {
+    header: string[],
+    body: TableBody[]
+  }
+  let tableData: TableData = {
+    header: [
+      'Departure',
+      'Destination'
+    ],
+    body: [
+      {
+        time: '14:48',
+        dest: 'Tromsø Lufthavn'
+      },
+      {
+        time: '13:37',
+        dest: 'Radisson Blu'
+      }
+    ]
+  }
+</script>
+
+<table class="border">
+  <thead>
+    <tr class="underline">
+      {#each tableData.header as data}
+        <th>{ data }</th>
+      {/each}
+    </tr>
+  </thead>
+  <tbody>
+    {#each tableData.body as body}
+      <tr>
+        <td>{ body.time }</td>
+        <td>{ body.dest }</td>
+      </tr>
+    {/each}
+  </tbody>
+</table>
+
+<style>
+  table {
+    margin: 3rem auto;
+    width: 100%;
+  }
+  th {
+    border-bottom: 1px solid #fff;
+    font-size: 2rem;
+    text-align: left;
+  }
+  td,
+  th {
+    padding: .6rem 1rem;
+  }
+  .underline {
+    border: 1px solid #fff;
+  }
+</style>
