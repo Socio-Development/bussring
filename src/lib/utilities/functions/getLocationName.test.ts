d2117ce3acb2d7ad66ec6860582f17ea68623b2a
CasperSocio <casper.tollefsen@mobit.no>
2021-10-25 03:50:56 +0200

    add test

---
+++ src/lib/utilities/functions/getLocationName.test.ts
@@ -0,0 +1,30 @@
+import type { IApiData } from '$lib/interfaces'
+import getLocationName from './getLocationName'
+
+test('Is able to retrieve StopPlace name', () => {
+  const apiCallData: IApiData = {
+    "data": {
+      "stopPlace": {
+        "name": "Scandic Ishavshotel",
+        "id": "NSR:StopPlace:52806",
+        "estimatedCalls": [
+          {
+            "expectedDepartureTime": "2021-10-25T06:30:00+0200",
+            "destinationDisplay": {
+              "frontText": "Tromsø lufthavn"
+            },
+            "serviceJourney": {
+              "line": {
+                "publicCode": "FB92",
+                "transportMode": "bus"
+              }
+            }
+          }
+        ]
+      }
+    }
+  }
+  expect(
+    getLocationName(apiCallData)
+  ).toBe("Scandic Ishavshotel")
+})
