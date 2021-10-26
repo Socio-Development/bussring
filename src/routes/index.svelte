7e5c46b40c18631cfcff00bfe90974d960bf1a41
Casper Tollefsen <casper.tollefsen@mobit.no>
2021-10-26 12:15:17 +0200

    update layout for index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container">
+  <h2>Bussring</h2>
+  <h1>Digital Signage Application</h1>
+  <p>Click one of the following locations to display its bus departures:</p>
+  <ul>
+    <li><a href="/table/52806">Scandic Ishavshotel</a></li>
+    <li><a href="/table/52281">Trosmø Lufthavn</a></li>
+  </ul>
+</div>
+
+<style>
+  .container {
+    color: #000;
+    margin: 0 auto;
+    max-width: 800px;
+  }
+</style>
